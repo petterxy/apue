37f54d195da2fdc6be615fdf1ff5c73d810b23fb
petter.wang <petter.xy@gmail.com>
2018-11-04 14:52:02 +0800

    CMSG_LEN----comlile problem

--- figlinks/17.21.sh
+++ figlinks/17.21.sh
@@ -1,1 +1,1 @@
-gcc  fig17.21.c  fig17.22.c fig17.23.c fig17.24.c error.c fig17.12.c fig17.13.c fig17.14.c writen.c -o opend
+gcc -std=gnu99 fig17.21.c  fig17.22.c fig17.23.c fig17.24.c error.c fig17.12.c fig17.13.c fig17.14.c writen.c -o opend
